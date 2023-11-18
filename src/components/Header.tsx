8b51941fb35a595f60425e34203afc862281aba4
Renan Gimenez <renangiimenez@gmail.com>
2023-11-18 13:03:57 -0300

    feat: implement header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,32 @@
-"use client"
+"use client";
 import { useContext } from "react";
 import { AuthContext } from "@/context/AuthContext";
+import ToggleThemeButton from "./ToggleThemeButton";
 
 export default function Header() {
-    const { signInWithGoogle, logOut } = useContext(AuthContext);
+  const { logOut } = useContext(AuthContext);
 
-    return (
-        <header className="h-16 w-full flex items-center justify-between bg-gray-900 shadow-lg px-[5%]">
-            <h1 className="font-bold">Chat | Firebase</h1>
+  const handleLogOut = async () => {
+    try {
+      await logOut();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-            <button 
-                className='border px-4 py-2 rounded-full'
-                onClick={signInWithGoogle}
-            >
-                Login With Google
-            </button>
-        </header>
-    );
+  return (
+    <header className="fixed h-16 w-full flex items-center justify-between bg-zinc-50 dark:bg-gray-900 shadow-lg px-[5%]">
+      <h1 className="font-bold">Chat | Firebase</h1>
+
+      <div className="flex items-center">
+        <ToggleThemeButton />
+        <button
+          className="border border-zinc-500/50 px-4 py-2 rounded-xl"
+          onClick={handleLogOut}
+        >
+          Log out
+        </button>
+      </div>
+    </header>
+  );
 }
