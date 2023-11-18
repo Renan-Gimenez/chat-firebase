713ba795df2250b828e7a18a19e3066a1f4d142d
Renan Gimenez <renangiimenez@gmail.com>
2023-11-17 23:57:32 -0300

    feat: implement dark mode

---
+++ src/context/ThemeContext.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { createContext, useEffect, useState } from "react";
+
+interface ThemeContextType {
+  theme: string;
+  toggleTheme: () => void;
+}
+
+export const ThemeContext = createContext<ThemeContextType>(
+  {} as ThemeContextType
+);
+
+export const ThemeProvider = ({ children }: any) => {
+  const inBrowser = typeof window !== "undefined";
+  const themeInLocalStorage = inBrowser ? localStorage.getItem("theme") : null;
+
+  const deviceTheme =
+    inBrowser && window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light";
+
+  const [theme, setTheme] = useState(themeInLocalStorage || deviceTheme);
+
+  const toggleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+      return;
+    }
+    document.documentElement.classList.remove("dark");
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
