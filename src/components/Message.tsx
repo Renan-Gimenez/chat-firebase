a91a3fa2e1c394a55cc92be11a6676a932985ad3
Renan Gimenez <renangiimenez@gmail.com>
2023-11-18 12:42:11 -0300

    feat: add message time

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,36 +1,50 @@
 interface Props {
-    createdAt: string;
-    profilePicture: string;
-    user: string;
-    text: string;
-    sendedByMe: boolean;
+  createdAt: string;
+  profilePicture: string;
+  user: string;
+  text: string;
+  userUid: string;
+  sendedByMe: boolean;
 }
 
-export default function Message({ createdAt, profilePicture, user, text, sendedByMe }:Props) {
-    const { seconds, nanoseconds }:any = createdAt;
+export default function Message({
+  createdAt,
+  profilePicture,
+  user,
+  text,
+  userUid,
+  sendedByMe,
+}: Props) {
+  const { seconds, nanoseconds }: any = createdAt;
 
-    const date = new Date(seconds * 1000 + nanoseconds / 1000000);
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
+  const date = new Date(seconds * 1000 + nanoseconds / 1000000);
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
 
-    const formattedTime = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
+  const formattedTime = `${hours.toString().padStart(2, "0")}:${minutes
+    .toString()
+    .padStart(2, "0")}`;
 
-    console.log(`Hora (24 horas no Brasil): ${formattedTime}`);
-
-    return (
-        <div className={`w-fit max-w-[400px] flex flex-row p-2 gap-2 rounded-xl bg-gray-100 dark:bg-gray-600 ${sendedByMe ? 'ml-auto bg-gray-200 dark:bg-gray-700' : ''}`}>
-            {!sendedByMe && 
-                <img
-                    alt="Avatar" 
-                    src={profilePicture} 
-                    className="h-10 rounded-full" 
-                />
-            }
-            <div className="flex flex-col">
-                {!sendedByMe && <span className="text-xs">{user}</span>}
-                <p>{text}</p>
-                <p className="text-xs text-gray-300 self-end">{formattedTime}</p>
-            </div>
-        </div>
-    );
+  return (
+    <div
+      className={`w-fit max-w-[80%] flex flex-row p-2 gap-2 rounded-xl bg-gray-100 dark:bg-gray-600 ${
+        sendedByMe ? "ml-auto bg-gray-200 dark:bg-gray-700" : ""
+      }`}
+    >
+      {!sendedByMe && (
+        <img
+          alt="Avatar"
+          src={`${profilePicture}`}
+          className="h-10 w-10 rounded-full"
+        />
+      )}
+      <div className="flex flex-col">
+        {!sendedByMe && <span className="text-xs">{user}</span>}
+        <p>{text}</p>
+        <p className="text-xs text-gray-500 dark:text-gray-300 self-end">
+          {formattedTime}
+        </p>
+      </div>
+    </div>
+  );
 }
