d636e792735a6de4f1716c13f721f06ba66c723a
Renan Gimenez <renangiimenez@gmail.com>
2024-01-04 23:29:52 -0300

    feat: implement not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <main className="min-h-screen w-screen flex items-center justify-center gap-16 px-[5%] dark:bg-gray-900">
+      <h1 className="text-lg">Error 404</h1>
+    </main>
+  );
+}
